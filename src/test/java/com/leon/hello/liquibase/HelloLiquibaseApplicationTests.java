e172e19f836749245f41633ac070826787d123e4
OceanLeonAI <beyondjayfir@126.com>
2022-03-29 22:04:40 +0800

    init the repository

---
+++ src/test/java/com/leon/hello/liquibase/HelloLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leon.hello.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloLiquibaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
